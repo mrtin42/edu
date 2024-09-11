1aeb8ab4039b3314a94a6cec666a2aa52110495d
mrtin42 <m.banksfrias@harriswimbledon.org.uk>
2024-09-11 13:46:51 +0100

    study room work (teach didnt show up LOL)

--- y12/OBJ01-Challenges/ballpit.py
+++ y12/OBJ01-Challenges/ballpit.py
@@ -1,1 +1,7 @@
-# just making this so i can work on it later as lunch time is nigh
+def pitVolume(rad,height):
+    return 3.14 * (rad*rad) * height
+
+def ballVolume(rad):
+    return (4/3) * 3.14 * (rad*rad*rad)
+
+print(f"The amount of balls needed to fit this pit is {int(pitVolume(float(input('what is the radius of the ball pit? ')),float(input('whar is the height of the ball pit? ')))/ballVolume(float(input('what is the radius of the balls? '))))}")
