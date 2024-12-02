c0d90eef3a8c0bb5f5be3f08f8b6fc20a4150256
mrtin42 <me@martin.blue>
2024-12-02 17:49:07 +0000

    totally real representation of the human brain

--- CW/2Dec2024 Object Oriented/classCreateExample.py
+++ CW/2Dec2024 Object Oriented/classCreateExample.py
@@ -1,10 +1,20 @@
-def __init__(self,
-             name="no name",
-             age=0,
-             nationality="no nationality",
-             gender = "no gender"):
-    self.name = name
-    self.age = age
-    self.nationality = nationality
-    self.gender = gender
+class Human():
+    def __init__(self,
+                name="no name",
+                age=0,
+                nationality="no nationality",
+                gender = "no gender"):
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+        self.gender = gender
+    
+    def speak(self):
+        print(f'Hi! My name is {self.name}.')
+
+
+bob = Human()
+
+print(bob.name)
+
 
